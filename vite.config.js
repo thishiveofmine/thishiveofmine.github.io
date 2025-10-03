c04eeda6e7056cc7ee8e2419cbe818459b6e92a0
Dmitry Solovyov <d.a.solovyov@gmail.com>
2025-10-03 12:22:26 +0200

    Vite base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { presetWind3 } from '@unocss/preset-wind3'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/thishiveofmine.github.io/',
+  base: '/',
   plugins: [
     react(),
     UnoCSS({
